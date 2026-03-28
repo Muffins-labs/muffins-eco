2f3d39d5025b0fbf21d8a06f279224339df43b6d
Arffei <arffei.storm@gmail.com>
2026-03-28 20:36:18 +0700

    666

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -3,7 +3,9 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    # Обрезаем пароль до 72 символов
+    truncated_password = password[:72]
+    return pwd_context.hash(truncated_password)
 
 def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password[:72], hashed_password)
